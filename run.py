28ab6874062e81283aa3b204a7d2c29efe905e51
Tanmyname <tanxzxzxzxz@gmail.com>
2024-11-29 20:57:27 +0800

    Update run.py

--- run.py
+++ run.py
@@ -9,4 +9,13 @@ if os.name == 'nt':
   os.system(pytubefix)
   os.system(requests)
   os.system(yt_dlp)
+else :
+  os.system('sudo apt install python3')
+  os.system('pkg install python3')
+  os.system('clear')
+  os.system(colorama)
+  os.system(pytubefix)
+  os.system(requests)
+  os.system(yt_dlp)
+  
   
