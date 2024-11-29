dbe4ec998f6724197b52a86c049f5beaa65989f6
Tanmyname <tanxzxzxzxz@gmail.com>
2024-11-29 20:55:20 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 import os 
-
-os.system('clear')
-os.system('pkg  update')
-os.system('pkg upgrade ')
-os.system('pkg  install python3')
-os.system('pip install  colorama')
-os.system('pip install pytubefix')
-os.system('pip install yt-dlp')
-os.system('pip install colorama')
-os.system('python3 main.py')
+colorama = 'pip install colorama'
+pytubefix = 'pip install pytubefix'
+requests = 'pip install requests'
+yt_dlp = 'pip install yt-dlp'
+if os.name == 'nt':
+  os.system('cls')
+  os.system(colorama)
+  os.system(pytubefix)
+  os.system(requests)
+  os.system(yt_dlp)
+  
